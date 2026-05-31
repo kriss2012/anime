aba1164d4422b5d867e360c5ca8b50d4d5f26ef6
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-05-31 09:47:39 +0530

    Update Navbar.jsx

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     <div className="nav">
       <div className="logo">Mend</div>
       <ul className="nav-links">
-        <li>Home</li>
-        <li>Support</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li><a href="#home">The Storm</a></li>
+        <li><a href="#struggle">The Struggle</a></li>
+        <li><a href="#healing">The Healing</a></li>
+        <li><a href="#growth">The Growth</a></li>
       </ul>
     </div>
   );
